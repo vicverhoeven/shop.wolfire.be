37bc7142dff20b9d2abac2b5668f21d5a44e9d9f
Vic-Verhoeven <Vic.verhoeven@student.ucll.be>
2025-02-07 00:13:08 +0100

    index fix

--- Public/scripts/main.js
+++ Public/scripts/main.js
@@ -23,7 +23,15 @@ fetch(`http://localhost:3000/api/product/${productId}`)
             <h1>${product.title}</h1>
             <p>${product.description}</p>
             <h2>Prijs: €${product.variants[0].price / 100}</h2>
-            <button class="add-to-cart">Toevoegen aan winkelwagen</button>
+            <button class="add-to-cart">Add to cart</button>
+            <form action="https://www.paypal.com/ncp/payment/CVXUYZQNCAGHW" method="post" target="_blank" class="paypal-form">
+                <input class="paypal-button" type="submit" value="Buy Now" />
+                <img src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg" alt="cards" />
+                <section>
+                    Powered by
+                    <img src="https://www.paypalobjects.com/paypal-ui/logos/svg/paypal-wordmark-color.svg" alt="paypal" style="height:0.875rem;vertical-align:middle;"/>
+                </section>
+            </form>
         </div>
     `;
 })
